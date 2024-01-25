b3d4cdc658b53255291bf1765c52ec7646ed66a5
OLEkSandrKoniaiev <oleksandrkoniaiev.r@gmail.com>
2024-01-25 23:38:15 +0200

    Create popup windows (messageboxes); Create password_generator function;

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from tkinter import *
+from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def password_generator():
-    pass
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = randint(8, 10)
+    nr_numbers = randint(2, 4)
+    nr_symbols = randint(2, 4)
+
+    password_letters = [choice(letters) for _ in range(nr_letters)]
+    password_numbers = [choice(numbers) for _ in range(nr_numbers)]
+    password_symbols = [choice(symbols) for _ in range(nr_symbols)]
+
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -12,15 +34,23 @@ def save_password():
     email = email_entry.get()
     password = password_entry.get()
 
-    # data_file = open("data.txt", "a")
-    # data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
-    # data_file.close()
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\n"
+                                                              f"Email: {email}\nPassword: {password}\n"
+                                                              f"Is it ok to save?")
 
-    # Щоб не писати close()
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+        if is_ok:
+            # data_file = open("data.txt", "a")
+            # data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
+            # data_file.close()
+
+            # Щоб не писати close()
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
